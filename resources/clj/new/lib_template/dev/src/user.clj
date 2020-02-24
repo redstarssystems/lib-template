2f15030b54df195480c0289e16c9afde9d6173f8
Mike Ananev <mike1452@yandex.ru>
2020-02-24 10:05:14 +0300

    init release

---
+++ resources/clj/new/lib_template/dev/src/user.clj
@@ -0,0 +1,14 @@
+(ns user)
+
+;; debug print with #p
+(require 'hashp.core)
+
+;; set global dev flag, which can be checked in runtime
+;; (when (resolve 'user/dev-mode) user/dev-mode)
+(def dev-mode true)
+
+(println (pr-str {:msg "development mode" :status (if (true? dev-mode) "on" "off")}) \newline)
+
+
+
+
---
+++ src/clj/new/lib_template.clj
@@ -0,0 +1,27 @@
+(ns clj.new.lib-template
+  (:require [clj.new.templates :refer [renderer project-data ->files]]))
+
+(defn lib-template
+  "entry point to run template."
+  [name]
+  (let [render (renderer "lib-template")
+        data   (project-data name)]
+    (println "Generating project from library template https://github.com/redstarssystems/lib-template.git")
+    (->files data
+      [".clj-kondo/config.edn" (render ".clj-kondo/config.edn" data)]
+      ["dev/src/user.clj" (render "dev/src/user.clj" data)]
+      ["resources/readme.txt" (render "resources/readme.txt" data)]
+      ["src/{{nested-dirs}}/core.clj" (render "src/core.clj" data)]
+      ["test/{{nested-dirs}}/core_test.clj" (render "test/core_test.clj" data)]
+      [".editorconfig" (render ".editorconfig" data)]
+      [".env" (render ".env" data)]
+      [".gitignore" (render ".gitignore" data)]
+      ["CHANGELOG.adoc" (render "CHANGELOG.adoc" data)]
+      ["deps.edn" (render "deps.edn" data)]
+      ["LICENSE" (render "LICENSE" data)]
+      ["Makefile" (render "Makefile" data)]
+      ["pbuild.edn" (render "pbuild.edn" data)]
+      ["README.adoc" (render "README.adoc" data)]
+      ["tests.edn" (render "tests.edn" data)]
+      )))
+
