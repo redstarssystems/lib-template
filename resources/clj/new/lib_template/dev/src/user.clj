5c692625cbbb2790a350a67abdf411bdaf758d82
Mike Ananev <mike1452@yandex.ru>
2020-03-01 14:36:39 +0300

    removed unnecessary code

--- resources/clj/new/lib_template/dev/src/user.clj
+++ resources/clj/new/lib_template/dev/src/user.clj
@@ -3,14 +3,6 @@
 ;; debug print with #p
 (require 'hashp.core)
 
-;; dynamic loading of libraries
-(require '[clj-deps.core :as deps])
-
-;; set global dev flag, which can be checked in runtime
-;; (when (resolve 'user/dev-mode) user/dev-mode)
-(def dev-mode true)
-
-(println (pr-str {:msg "development mode" :status (if (true? dev-mode) "on" "off")}) \newline)
 
 
 
